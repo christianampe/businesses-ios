1ed6dd906253640c9dd08ed6d1063263253827eb
christianampe <christianampe.work@gmail.com>
2019-05-23 19:58:42 -0700

    began organizing the project

---
+++ WeedmapsChallenge/Sources/Screens/Launch/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2018 Weedmaps, LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
