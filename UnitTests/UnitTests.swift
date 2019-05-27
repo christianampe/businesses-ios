7d264cfb54e148da34f26a307cad0f3606030466
christianampe <christianampe.work@gmail.com>
2019-05-27 12:04:19 -0700

    added tests

---
+++ UnitTests/UnitTests.swift
@@ -0,0 +1,26 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Christian Ampe on 5/27/19.
+//  Copyright © 2019 Weedmaps, LLC. All rights reserved.
+//
+
+import XCTest
+
+class UnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
